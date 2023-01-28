221b8ae539ca85b5d6b3a5a8014058acd80c9af1
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-01-28 11:25:13 +0530

    button --helper

---
+++ src/helper/Button.jsx
@@ -0,0 +1,14 @@
+const Button = ({ btnName, className }) => {
+  return (
+    <>
+      <button
+        className={`block rounded-lg bg-buttonColor px-5 py-3 text-sm font-medium text-buttonText transition hover:bg-[#eb7724] focus:outline-0 ${className}`}
+        type="button"
+      >
+        {btnName}
+      </button>
+    </>
+  );
+};
+
+export default Button;
