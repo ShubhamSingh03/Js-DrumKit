4d1236d8869f7a11f636b51b148f48c374714cf4
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-01-28 11:17:02 +0530

    react-router-dom --config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import "./App.css";
 
+// routes
+import { Routes, Route } from "react-router-dom";
+
+//layout
+import PageLayout from "./layout/PageLayout";
+
+// pages
+import Home from "./pages/Home";
+import Tutorial from "./pages/Tutorial";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
   return (
-    <div className="App">
-      <h1>This is a Drum Kit</h1>
-    </div>
+    <>
+      <Routes>
+        <Route element={<PageLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/tutorial" element={<Tutorial />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
   );
 }
 
