3eddbf341e83597b19b3a72892de7f4169a6c2bb
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-01-29 20:55:21 +0530

    pageLayout updated

--- src/layout/PageLayout.jsx
+++ src/layout/PageLayout.jsx
@@ -4,6 +4,12 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
+/****************************************
+ *
+ * Page Layout
+ *
+ ***************************************/
+
 const PageLayout = () => (
   <>
     <Navbar />
