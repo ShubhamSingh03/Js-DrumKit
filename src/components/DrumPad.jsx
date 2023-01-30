e6ada77cc7cbc26db73530621e3bed968f7fdd77
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-01-30 23:16:10 +0530

    handleKeyPress --keyboard keypress animation

--- src/components/DrumPad.jsx
+++ src/components/DrumPad.jsx
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from "react";
 
 const DrumPad = (props) => {
+  const [keyPress, setKeyPress] = useState(false);
+
+  /*******************************************
+   *
+   *  on onClick event on button
+   *
+   ******************************************* */
+
   const handleClick = () => {
     if (props.isPowerOn) {
       const sound = new Audio(props.sound);
@@ -11,26 +19,63 @@ const DrumPad = (props) => {
     }
   };
 
-  const handleKeyPress = (event) => {
+  /*******************************************
+   *
+   *  keyboard key press -down event
+   *
+   ******************************************* */
+
+  const handleKeyPressDown = (event) => {
+    if (event.key.toUpperCase() === props.keyCode) {
+      if (props.isPowerOn) {
+        const sound = new Audio(props.sound);
+        sound.volume = props.volume;
+        setKeyPress(!keyPress);
+        sound.play();
+      } else {
+        window.alert("Drum is OFF! Toggle to ON");
+      }
+    }
+  };
+
+  /*******************************************
+   *
+   *  keyboard key press -up event
+   *
+   ******************************************* */
+
+  const handleKeyPressUp = (event) => {
     if (event.key.toUpperCase() === props.keyCode && props.isPowerOn) {
-      const sound = new Audio(props.sound);
-      sound.volume = props.volume;
-      sound.play();
+      setKeyPress(!keyPress);
     }
   };
 
   useEffect(() => {
-    document.addEventListener("keydown", handleKeyPress);
+    document.addEventListener("keydown", handleKeyPressDown);
+    document.addEventListener("keyup", handleKeyPressUp);
     return () => {
-      document.removeEventListener("keydown", handleKeyPress);
+      document.removeEventListener("keydown", handleKeyPressDown);
+      document.removeEventListener("keyup", handleKeyPressUp);
     };
-  }, [props.isPowerOn]);
+  }, [props.isPowerOn, keyPress]);
 
   return (
     <>
+      {/*******************************************
+       *
+       *  button for playing sound
+       *
+       ******************************************* */}
+
       <button
         onClick={handleClick}
-        className={`drum-pad inline-block border rounded-lg bg-buttonColor py-1 px-3 text-buttonText font-semibold  hover:border-buttonColor hover:scale-105  hover:transition-all hover:duration-100 hover:ease-in-out hover:backdrop-blur hover:bg-glassButtonBg hover:shadow-buttonShadow focus:outline-0 focus:ring-offset-0 focus:outline-buttonColor sm:py-2 sm:px-4 ${props.className}`}
+        className={`${
+          keyPress
+            ? "scale-75 transition-all duration-100 ease-in-out bg-glassButtonBg shadow-volumeSliderRangeShadow border border-buttonColor"
+            : ""
+        } drum-pad inline-block border rounded-lg bg-buttonColor py-1 px-3 text-buttonText font-semibold  hover:border-buttonColor hover:scale-105  hover:transition-all hover:duration-100 hover:ease-in-out hover:backdrop-blur hover:bg-glassButtonBg hover:shadow-volumeSliderRangeShadow focus:outline-0 focus:ring-offset-0 focus:outline-buttonColor sm:py-2 sm:px-4 ${
+          props.className
+        }`}
       >
         {props.name}
       </button>
