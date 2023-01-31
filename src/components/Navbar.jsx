1c241403ee839a89517603ae198ddbe356ae3e1b
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-01-31 17:51:28 +0530

    modal integrated to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,39 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
-import { useState } from "react";
 
-// button helper
+//Button Helper --import
 import Button from "../helper/Button";
 
-// navbar component
+// Navbar Modal Component --import
+import NavModal from "./NavModal";
+
+/*******************************************
+ *
+ * Navbar Component
+ *
+ *******************************************/
+
 const Navbar = () => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  /***************************************
+   *  handleModal function
+   **************************************/
+  const handleModal = () => {
+    setIsOpen(!modalIsOpen);
+  };
+
+  /***************************************
+   *  useLocation hook react-router-dom
+   **************************************/
   const { pathname } = useLocation();
 
   return (
     <>
+      {/***************************************
+       *  Navbar
+       **************************************/}
       <nav aria-label="Page Header">
         <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-4 sm:px-6 lg:px-8">
           <div className="sm:flex sm:items-center sm:justify-between">
@@ -28,9 +51,9 @@ const Navbar = () => {
             </div>
             {/* buttons/links to pages */}
             <div className="mt-4 flex flex-col gap-4 sm:mt-0 sm:flex-row sm:items-center">
-              {/* project link */}
-
+              {/* yt video modal */}
               <button
+                onClick={handleModal}
                 className="inline-flex items-center justify-center rounded-lg border border-gray-400 px-5 py-3 text-paragraph transition hover:text-gray-700 focus:outline-none focus:ring"
                 type="button"
               >
@@ -39,18 +62,31 @@ const Navbar = () => {
                   Watch YouTube Demo{" "}
                 </span>
               </button>
-
-              {/* modal component */}
-
-              {/* tutorial page */}
-              {pathname == "/" ? (
-                <Link to="/tutorial">
-                  <Button btnName={"How to Play?"} className="w-full" />
-                </Link>
+              {/* modal component & routes pages */}
+              {pathname === "/" ? (
+                <>
+                  {modalIsOpen ? (
+                    <NavModal
+                      handleModal={handleModal}
+                      videoId={"enKgl7wf5cM"}
+                    />
+                  ) : null}
+                  <Link to="/tutorial">
+                    <Button btnName={"How to Play?"} className="w-full" />
+                  </Link>
+                </>
               ) : (
-                <Link to="/">
-                  <Button btnName={"Back to Home"} className="w-full" />
-                </Link>
+                <>
+                  {modalIsOpen ? (
+                    <NavModal
+                      handleModal={handleModal}
+                      videoId={"Dnf59hzpRsU"}
+                    />
+                  ) : null}
+                  <Link to="/">
+                    <Button btnName={"Back to Home"} className="w-full" />
+                  </Link>
+                </>
               )}
             </div>
           </div>
